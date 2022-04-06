6872b41999bd9947e1e1f981800039a046aa58b3
lab507_win <yfh97@qq.com>
2022-04-06 15:12:25 +0800

    spring-redis整合测试

---
+++ springboot08-redis/src/main/java/com/yfh/springboot08redis/Springboot08RedisApplication.java
@@ -0,0 +1,13 @@
+package com.yfh.springboot08redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot08RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot08RedisApplication.class, args);
+    }
+
+}
---
+++ springboot08-redis/src/test/java/com/yfh/springboot08redis/Springboot08RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yfh.springboot08redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot08RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
